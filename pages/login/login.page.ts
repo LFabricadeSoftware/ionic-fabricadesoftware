5aa853f1f79c1b1c7361aa53298358993cc4f136
LFabricadeSoftware <lfabsoftware@gmail.com>
2020-04-17 02:03:00 -0300

    1 file added

---
+++ pages/login/login.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { User } from 'src/app/interfaces/user';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  
+     
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 50;
+  public userLogin: User = {};
+  public userCadastro: User = {};
+
+
+  constructor(public keyboard: Keyboard) { }
+
+  ngOnInit() {
+    
+  }
+  
+  login(){
+
+  }
+  cadastro(){
+    console.log(this.userCadastro);
+  }
+
+}
