da8463b11e28dfc84bc474d9a1c955abf13bc862
LFabricadeSoftware <lfabsoftware@gmail.com>
2020-04-17 03:07:21 -0300

    4 files modified, 6 added

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,43 +3,43 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},  
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule'},   
+  { path: 'login', loadChildren: '../pages/login/login.module#LoginPageModule'},/*,  
+  { path: 'menu', loadChildren: '../pages/menu/menu.module#MenuPageModule'},   
   { path: 'details', loadChildren:  './pages/todo-details/todo-details.module#TodoDetailsPageModule'},
   { path: 'details/:id', loadChildren:  './pages/todo-details/todo-details.module#TodoDetailsPageModule'},
   { path: 'flutter', loadChildren:  './pages/flutter/flutter.module#FlutterPageModule'},
   { path: 'ionic', loadChildren:  './pages/ionic/ionic.module#IonicPageModule'},
   { path: 'ionic/:id', loadChildren:  './pages/ionic/ionic.module#IonicPageModule'},
   { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule'},
-  { path: 'cadastro/:id', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule'},
+  { path: 'cadastro/:id', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule'},*/
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
+    loadChildren: () => import('../pages/login/login.module').then( m => m.LoginPageModule)
+  }/*,
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('../pages/register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./members/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+    loadChildren: () => import('../members/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./members/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  },
+    loadChildren: () => import('../members/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  }*/,
   {
     path: 'autentication',
-    loadChildren: () => import('./services/authentication.service').then( m => m.AuthenticationService)
+    loadChildren: () => import('../services/authentication.service').then( m => m.AuthenticationService)
   },
   {
     path: 'auth-guard',
-    loadChildren: () => import('./services/auth-guard.service').then( m => m.AuthGuardService)
-  },
+    loadChildren: () => import('../services/auth-guard.service').then( m => m.AuthGuardService)
+  }/*,
   {
     path: 'cadastro',
-    loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
-  }
+    loadChildren: () => import('../pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  }*/
   
 
 ];
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { IonicApp, IonicModule, IonicErrorHandler,IonicRouteStrategy  } from 'ionic-angular';
+import { RouteReuseStrategy } from '@angular/router';
+
+
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -9,27 +20,21 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Keyboard
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
